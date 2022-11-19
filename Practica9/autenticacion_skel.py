37deb7c2eed3270b75ac6a7099e03cb7bd56a3ca
Reven02 <drevenga@ucm.es>
2022-11-19 19:15:43 +0100

    Apartados 1.1 y 1.3 de la p9 hechos

--- Practica9/autenticacion_skel.py
+++ Practica9/autenticacion_skel.py
@@ -1,13 +1,16 @@
 # -*- coding: utf-8 -*-
 
-#
-# CABECERA AQUI
-#
+# GIW 2022-23
+# Práctica 09
+# Grupo 02
+# Autores: Diego Revenga González, Raul Blas Ruiz, Jorge Bello Martin, Eva Lucas Leiro
 
 
 from flask import Flask, request, session, render_template
 from mongoengine import connect, Document, StringField, EmailField
 # Resto de importaciones
+import random, string
+from argon2 import PasswordHasher, profiles
 
 
 app = Flask(__name__)
@@ -36,7 +39,30 @@ class User(Document):
 
 @app.route('/signup', methods=['POST'])
 def signup():
-    ...
+    nickname = request.form['nickname']
+    full_name = request.form['full_name']
+    country = request.form['country']
+    email = request.form['email']
+    password = request.form['password']
+    password2 = request.form['password2']
+
+    # Si las contraseñas no coinciden:
+    if password != password2:
+        return render_template('passwordsDontMatch.html')
+
+    # Si ya existe el nickname:
+    if [u for u in User.objects() if u.user_id == nickname]:
+        return render_template('alreadyExistingNickname.html')
+
+    # Encriptar password
+    ph = PasswordHasher().from_parameters(profiles.RFC_9106_LOW_MEMORY)
+    passEncrypted = ph.hash(password)
+
+    # Guardar usuario en la base de datos
+    new_user = User(user_id=nickname, full_name=full_name, country=country, email=email, passwd=passEncrypted)
+    new_user.save()
+
+    return render_template('welcomeUser.html', name = full_name)
 
 
 @app.route('/change_password', methods=['POST'])
@@ -46,8 +72,24 @@ def change_password():
            
 @app.route('/login', methods=['POST'])
 def login():
-    ...
+    nickname = request.form['nickname']
+    password = request.form['password']
     
+    # Si el usuario no existe:
+    if not (user := [u for u in User.objects() if u.user_id == nickname]):
+        return render_template('wrongCredentials.html')
+
+    saved_hash = user[0].passwd
+    full_name = user[0].full_name
+
+    ph = PasswordHasher().from_parameters(profiles.RFC_9106_LOW_MEMORY)
+
+    try:
+        ph.verify(saved_hash, password)
+        return render_template('welcomeUser.html', name = full_name)
+    except:
+        return render_template('wrongCredentials.html')
+
 
 ##############
 # APARTADO 2 #
@@ -82,5 +124,7 @@ class FlaskConfig:
 
 
 if __name__ == '__main__':
+    # User.drop_collection()
+
     app.config.from_object(FlaskConfig())
     app.run()
